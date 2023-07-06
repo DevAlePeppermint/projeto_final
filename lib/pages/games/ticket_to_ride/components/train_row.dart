1994827ac30083ca42304446bd6b08effd674ca9
Alexandra Gropp <xandagropp@hotmail.com>
2023-07-06 20:11:15 -0300

    Cria contador

--- lib/pages/games/ticket_to_ride/components/train_row.dart
+++ lib/pages/games/ticket_to_ride/components/train_row.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 import '../../../../components/text_secondary.dart';
 
-class TrainRow extends StatelessWidget {
+class TrainRow extends StatefulWidget {
   final int trainsUnits;
+  final Function() onPlus;
+  final Function() onDown;
 
   const TrainRow({
     super.key,
-    required this.trainsUnits  
+    required this.trainsUnits,
+    required this.onPlus,
+    required this.onDown,
   });
+
+  @override
+  State<TrainRow> createState() => _TrainRowState();
+}
+
+class _TrainRowState extends State<TrainRow> {
+  int points = 0;
+
+  void onClickPlus () {
+    setState(() {
+      points = points + 1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -15,36 +32,37 @@ class TrainRow extends StatelessWidget {
       padding: const EdgeInsets.all(2),
       child: Row(
         children: [
-          for (var i = 0; i < trainsUnits; i++)
+          for (var i = 0; i < widget.trainsUnits; i++)
             SizedBox(
               height: 37,
               width: 37,
               child: Image.asset('lib/assets/trem-de-brinquedo.png')
             ),
-          Spacer(),
+          const Spacer(),
           Container(
-            margin: EdgeInsets.symmetric(horizontal: 4),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: IconButton(
-              onPressed: () {}, 
-              icon: Icon(Icons.add, color: Colors.blue)
-            )
-          ),
-          TextSecondary(text:'0'),
-          Container(
-            margin: EdgeInsets.symmetric(horizontal: 4),
+            margin: const EdgeInsets.symmetric(horizontal: 4),
             decoration: BoxDecoration(
               color: Colors.white,
               borderRadius: BorderRadius.circular(8),
             ),
             child: IconButton(
               focusColor: Colors.pink.withOpacity(0.3),
-              onPressed: () {},
-              icon: Icon(Icons.remove,  color: Colors.red)
+              onPressed: widget.onDown,
+              icon: const Icon(Icons.remove,  color: Colors.red)
             )
+          ),
+          TextSecondary(text: points.toString()),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 4),
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(8),
+            ),
+            child: IconButton(
+              onPressed: onClickPlus, 
+              icon: const Icon(Icons.add, color: Colors.blue)
+            )
+            
           )
         ],
       ),
