06754b006c7cecaf7d1fbea8ae8659b34d8e1bc3
Alexandra Gropp <xandagropp@hotmail.com>
2023-07-07 12:57:57 -0300

    Adiciona tela de ranking

--- lib/app_routes.dart
+++ lib/app_routes.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_final/pages/games/ticket_to_ride/ticket_to_ride.dart';
 import 'package:projeto_final/pages/select_game/select_game.dart';
+import 'pages/ranking/ranking.dart';
 
 class AppRoutes {
   static String selecionaJogo = '/';
   static String ticketToRide = '/seleciona-jogo/ticket-to-ride';
+  static String ranking = '/ranking';
 
   static Map<String, Widget Function(BuildContext)> routes () {
       return {
         ticketToRide: (context) => const TicketToRideScore(),
         selecionaJogo: (context) => const SelecionaJogo(),
+        ranking: (context) => const Ranking(),
     };
   }
 }
---
+++ lib/pages/ranking/ranking.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:projeto_final/components/button.dart';
+import 'package:projeto_final/components/text_header.dart';
+import 'package:projeto_final/app_routes.dart';
+import 'package:projeto_final/providers/ticket_to_ride_provider.dart';
+import 'package:provider/provider.dart';
+
+class Ranking extends StatefulWidget {
+  const Ranking({super.key});
+
+  @override
+  State<Ranking> createState() => _RankingState();
+}
+
+class _RankingState extends State<Ranking> {
+  late TicketToRideProvider store;
+
+  void navigateToHome() {
+    Navigator.pushNamed(context, AppRoutes.selecionaJogo);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    store = Provider.of<TicketToRideProvider>(context, listen: false);
+
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Header(text: 'Ranking'),
+          const SizedBox(
+            height: 24,
+          ),
+          const Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Text('Jogador a'),
+              // Text('Jogador b'),
+              // Text('Jogador c'),
+            ],
+          ),
+          Container(
+            width: 300,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(5),
+              color: Colors.amber[50]
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+              children: [
+                const Header(text: 'Pontuação'),
+                const SizedBox(
+                  height: 24,
+                ),
+                for(var index = 0; index < store.players.length; index++)
+                Row(
+                  children: [
+                    Text(store.players[index].player),
+                    const Spacer(),
+                    Text(store.players[index].totalPoints.toString())
+                  ],
+                ),
+              ]),
+            ),
+          ),
+          const SizedBox(
+            height: 16,
+          ),
+          Column(children: [
+            Button(
+              text: 'jogar novamente',
+              onPressed: () {}
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Button(
+              text: 'pagina inicial',
+              onPressed: navigateToHome
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+          ],)
+        ],
+      ),
+    );
+  }
+}
